e4138eb1b555bb64efb355f255487a64d4a4175a
Ryan Whelan <rwhelan@osxs-MacBook-Pro.local>
2021-12-07 22:19:16 -0500

    Day-04: Problem One

--- Day-04/main.go
+++ Day-04/main.go
@@ -5,16 +5,102 @@ import (
 	"fmt"
 	"io/ioutil"
 	"strconv"
+	"strings"
 )
 
 type Space struct {
 	X, Y, Value int
+	Marked      bool
 }
 
 type Board struct {
 	spaces  [5][5]*Space
 	idx     map[int]*Space
 	histroy []int
+}
+
+func (b *Board) Print() string {
+	resp := strings.Builder{}
+	for _, row := range b.spaces {
+		resp.WriteString(
+			fmt.Sprintf(
+				"%2d %2d %2d %2d %2d\n",
+				row[0].Value, row[1].Value, row[2].Value, row[3].Value, row[4].Value,
+			),
+		)
+	}
+
+	return resp.String()
+}
+
+func (b *Board) EvalDraw(value int) *Board {
+	if b.IsWinner() {
+		return b
+	}
+
+	space, ok := b.idx[value]
+	if ok && !space.Marked {
+		space.Marked = true
+		b.histroy = append(b.histroy, value)
+	}
+
+	return b
+}
+
+func (b *Board) UnmarkedNumbers() []int {
+	resp := make([]int, 0)
+	for _, space := range b.idx {
+		if !space.Marked {
+			resp = append(resp, space.Value)
+		}
+	}
+
+	return resp
+}
+
+func (b *Board) IsWinner() bool {
+	all := func(spaces ...*Space) bool {
+		for _, s := range spaces {
+			if !s.Marked {
+				return false
+			}
+		}
+
+		return true
+	}
+
+	for i := 0; i < 5; i++ {
+		if all(
+			b.spaces[i][0],
+			b.spaces[i][1],
+			b.spaces[i][2],
+			b.spaces[i][3],
+			b.spaces[i][4],
+		) {
+			return true
+		}
+
+		if all(
+			b.spaces[0][i],
+			b.spaces[1][i],
+			b.spaces[2][i],
+			b.spaces[3][i],
+			b.spaces[4][i],
+		) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IntSum(values []int) int {
+	resp := 0
+	for _, v := range values {
+		resp += v
+	}
+
+	return resp
 }
 
 func ParseBoard(input []byte) *Board {
@@ -82,12 +168,26 @@ func ParseInput(filename string) ([]int, []*Board, error) {
 	return draws, boards, nil
 }
 
+func problemOne(draws []int, boards []*Board) {
+	for _, d := range draws {
+		for _, b := range boards {
+			b.EvalDraw(d)
+			if b.IsWinner() {
+				fmt.Println(
+					"Problem One:",
+					b.histroy[len(b.histroy)-1]*IntSum(b.UnmarkedNumbers()),
+				)
+				return
+			}
+		}
+	}
+}
+
 func main() {
 	draws, boards, err := ParseInput("input")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%+v", boards)
-	fmt.Println(draws)
+	problemOne(draws, boards)
 }
