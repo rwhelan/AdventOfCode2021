02c93fba3285d4cd9816aa85c3d8d1be8bc5c94b
Ryan Whelan <rwhelan@osxs-MacBook-Pro.local>
2021-12-13 13:42:57 -0500

    Day-06: Little Cleanup

--- Day-06/main.go
+++ Day-06/main.go
@@ -7,13 +7,6 @@ import (
 	"strconv"
 )
 
-// func newMap() map[int]int {
-// 	return map[int]int{
-// 		0: 0, 1: 0, 2: 0, 3: 0, 4: 0,
-// 		5: 0, 6: 0, 7: 0, 8: 0,
-// 	}
-// }
-
 func ReadInput(filename string) (map[int]int, error) {
 	rawInput, err := ioutil.ReadFile(filename)
 	if err != nil {
