6f04999f8f433439dd3fe6695660185dbc84f02c
Ryan Whelan <rwhelan@osxs-MacBook-Pro.local>
2021-12-13 13:42:26 -0500

    Day-06: Problems One and Two

---
+++ Day-06/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+)
+
+// func newMap() map[int]int {
+// 	return map[int]int{
+// 		0: 0, 1: 0, 2: 0, 3: 0, 4: 0,
+// 		5: 0, 6: 0, 7: 0, 8: 0,
+// 	}
+// }
+
+func ReadInput(filename string) (map[int]int, error) {
+	rawInput, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	if rawInput[len(rawInput)-1] == '\n' {
+		rawInput = rawInput[:len(rawInput)-1]
+	}
+
+	sections := bytes.Split(rawInput, []byte{','})
+	resp := make(map[int]int)
+
+	for _, v := range sections {
+		num, err := strconv.Atoi(string(v))
+		if err != nil {
+			return nil, err
+		}
+
+		resp[num]++
+	}
+
+	return resp, nil
+}
+
+func solve(initial map[int]int, gen int) int {
+	fish := make(map[int]int)
+	for k, v := range initial {
+		fish[k] = v
+	}
+
+	for i := 0; i < gen; i++ {
+		nfish := make(map[int]int)
+		for k, v := range fish {
+			if k == 0 {
+				nfish[8] += fish[0]
+				nfish[6] += fish[0]
+			} else {
+				nfish[k-1] += v
+			}
+		}
+
+		fish = nfish
+	}
+
+	sum := 0
+	for _, v := range fish {
+		sum += v
+	}
+
+	return sum
+}
+
+func main() {
+	// fish := []int{3, 4, 3, 1, 2}
+	fish, err := ReadInput("input")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Problem One:", solve(fish, 80))
+	fmt.Println("Problem Two:", solve(fish, 256))
+}
