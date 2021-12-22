d1a381a7ff4a3322b26a33a5b91872a46a9e90d8
Ryan Whelan <rwhelan@osxs-MacBook-Pro.local>
2021-12-22 14:03:56 -0500

    Day-12: Problem Two

--- Day-12/main.go
+++ Day-12/main.go
@@ -59,20 +59,6 @@ func isSmallCave(node *graph.Node) bool {
 	return true
 }
 
-func GraphWalk(path *graph.Path, nextNode *graph.Node) graph.WalkerInstruction {
-	if path.CurrentNode().Name == "end" {
-		return graph.PATH_STOP
-	}
-
-	for _, node := range path.Nodes {
-		if nextNode == node && isSmallCave(node) {
-			return graph.PATH_DROP
-		}
-	}
-
-	return graph.PATH_CONTINUE
-}
-
 func problemOne(g *graph.Graph) int {
 	walkfunc := func(path *graph.Path, nextNode *graph.Node) graph.WalkerInstruction {
 		if path.CurrentNode().Name == "end" {
@@ -96,6 +82,65 @@ func problemOne(g *graph.Graph) int {
 	return paths.Len()
 }
 
+func nodeCount(path *graph.Path, node *graph.Node) int {
+	resp := 0
+	for _, n := range path.Nodes {
+		if n.Name == node.Name {
+			resp++
+		}
+	}
+
+	return resp
+}
+
+func problemTwo(g *graph.Graph) int {
+	smallCaveNames := make([]string, 0)
+	for _, node := range g.AllNodes() {
+		if isSmallCave(node) && node.Name != "start" && node.Name != "end" {
+			smallCaveNames = append(smallCaveNames, node.Name)
+		}
+	}
+
+	walkFuncs := make([]func(*graph.Path, *graph.Node) graph.WalkerInstruction, len(smallCaveNames))
+	for i, cname := range smallCaveNames {
+		smallCaveName := cname
+
+		walkFuncs[i] = func(path *graph.Path, nextNode *graph.Node) graph.WalkerInstruction {
+			if path.CurrentNode().Name == "end" {
+				return graph.PATH_STOP
+			}
+
+			if nextNode.Name == smallCaveName && nodeCount(path, nextNode) == 2 {
+				return graph.PATH_DROP
+			}
+
+			for _, node := range path.Nodes {
+				if nextNode == node && isSmallCave(node) && nextNode.Name != smallCaveName {
+					return graph.PATH_DROP
+				}
+			}
+
+			return graph.PATH_CONTINUE
+		}
+	}
+
+	allPaths := graph.NewPathList()
+	for _, f := range walkFuncs {
+		paths, err := g.Walk(g.GetNode("start"), f)
+		if err != nil {
+			panic(err)
+		}
+
+		allPaths.Add(paths)
+
+	}
+
+	allPaths.Dedup()
+
+	return allPaths.Len()
+
+}
+
 func main() {
 	g, err := ParseGraph("input")
 	if err != nil {
@@ -103,4 +148,5 @@ func main() {
 	}
 
 	fmt.Println("Problem One:", problemOne(g))
+	fmt.Println("Problem Two:", problemTwo(g))
 }
